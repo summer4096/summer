04ce5c56e98804311d0344b6a53bd36a6bf0e2fd
Summer <github@summer.dev>
2023-01-26 21:41:09 -0800

    hi I'm Kat

--- fruitOS.js
+++ fruitOS.js
@@ -1,6 +1,38 @@
 /**
  * Fruit OS system homepage
  */
+
+function bindDockItem(className, callback) {
+  const dockIcon = document.querySelector('.fruitOS .desktop .dock .dock-icon.' + className)
+
+  let isOpen = false;
+
+  dockIcon.addEventListener('click', async () => {
+    if (isOpen) return
+    isOpen = true
+
+    const dockIconRect = dockIcon.getBoundingClientRect();
+    const xOrigin = dockIconRect.left + (dockIconRect.width / 2)
+    const yOrigin = dockIconRect.top + (dockIconRect.height / 2)
+  
+    const desktopMainRect = document.querySelector('.desktop .main').getBoundingClientRect();
+    const xTarget = desktopMainRect.width / 2
+    const yTarget = desktopMainRect.height / 2
+  
+    dockIcon.classList.add('bounce')
+  
+    const positioner = document.querySelector('.fruitOS .window-positioner.' + className)
+    positioner.style.transform = `translate(${xOrigin}px, ${yOrigin}px)`
+  
+    await sleep(500);
+  
+    positioner.classList.add('launching')
+    positioner.style.transform = `translate(${xTarget}px, ${yTarget}px)`
+  
+    callback();
+  })
+}
+
 async function runFruitOS() {
   document.querySelector('.initial-homepage').classList.add('hide')
   document.querySelector('.terminal').classList.add('hide')
@@ -22,7 +54,7 @@ async function runFruitOS() {
   const progress = bootScreen.querySelector('.progress-bar .fill')
   progress.style.width = '100%'
 
-  await sleep(5500)
+  await sleep(3500)
 
   bootScreen.classList.remove('stage-2')
 
@@ -48,30 +80,6 @@ async function runFruitOS() {
   await sleep(500)
 
   document.querySelector('.fruitOS .desktop .dock').classList.add('visible')
-
-  await sleep(1000)
-
-  const dockIcon = document.querySelector('.fruitOS .desktop .dock .dock-icon:first-child')
-
-  const dockIconRect = dockIcon.getBoundingClientRect();
-  const xOrigin = dockIconRect.left + (dockIconRect.width / 2)
-  const yOrigin = dockIconRect.top + (dockIconRect.height / 2)
-
-  const desktopMainRect = document.querySelector('.desktop .main').getBoundingClientRect();
-  const xTarget = desktopMainRect.width / 2
-  const yTarget = desktopMainRect.height / 2
-
-  dockIcon.classList.add('bounce')
-
-  const positioner = document.querySelector('.fruitOS .window-positioner')
-  positioner.style.transform = `translate(${xOrigin}px, ${yOrigin}px)`
-
-  await sleep(4000);
-
-  positioner.classList.add('launching')
-  positioner.style.transform = `translate(${xTarget}px, ${yTarget}px)`
-
-  await sleep(600)
-
-  photoBooth()
 }
+
+bindDockItem('zombo-com', () => {})
